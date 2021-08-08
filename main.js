2cec2f3609bd0cd4530a6964f6ac729fd109bd42
Troy Dorman <troydorman@gmail.com>
2021-08-07 19:37:22 -0600

    defined some variables for our two main classes and added event listeners for fill

---
+++ main.js
@@ -0,0 +1,6 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll("empty");
+
+// fill listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
