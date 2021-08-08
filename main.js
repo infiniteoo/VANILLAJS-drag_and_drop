1d688bbe312f6fed18281e2111cf05e6ea8837d6
Troy Dorman <troydorman@gmail.com>
2021-08-07 19:44:06 -0600

    implemented the dragStart function

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const empties = document.querySelectorAll("empty");
 // fill listeners
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
+
+// drag functions
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
